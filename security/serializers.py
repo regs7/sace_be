85d2d00b4b46ef7a6225ffdabaec5721063c6afa
Jose Gabriel Giron <jgabrielsk8@gmail.com>
2020-05-24 11:55:27 -0600

    return auth data

--- security/serializers.py
+++ security/serializers.py
@@ -5,7 +5,10 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
     def validate(self, attrs):
         data = super(CustomTokenObtainPairSerializer, self).validate(attrs)
+        user = self.user
         data.update({
-            'groups': [{'name': user_group.name} for user_group in self.user.groups.all()]
+            'groups': [user_group.name for user_group in user.groups.all()],
+            'authenticated': True,
+            'email': user.email
         })
         return data
